f052c7dbaa4056e41f985662246078af96c18916
Yenphuong Tong <yenphuongtong@yenphuongs-mbp.lan>
2022-08-07 01:46:32 -0700

    updating dependencies and added code for connecting to database

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express')
-const app = express()
-const PORT = 3001
+const mongoose = require('mongoose')
+
+// CONFIGURATION
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// CONNECTING TO DATABASE
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(
+  MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true,}
+);
+
+let connectionDB = mongoose.connection
+
+connectionDB.on(`connected`, () => {
+    console.log('Success')
+})
+
+connectionDB.on(`error`, () => {
+    console.log('Fail')
+})
 
 app.get('/', (req, res) => res.send('Hello World'))
-app.listen(PORT, () => console.log(`App running on ${PORT}`))
+app.listen(PORT, () => console.log(`App running on ${PORT}`)) 
+
