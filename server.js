8cb9f33a95b2682b41734414ade6142bfdee2142
Yenphuong Tong <yenphuongtong@Yenphuongs-MacBook-Pro.local>
2022-08-06 18:53:37 -0700

    node basic setup installed

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(PORT, () => console.log(`App running on ${PORT}`))
