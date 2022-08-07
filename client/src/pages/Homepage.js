ffcf5bf1f0e8045dc4917f9a847976dd1bb9d569
Yenphuong Tong <yenphuongtong@Yenphuongs-MacBook-Pro.local>
2022-08-06 18:50:04 -0700

    front end basic stucture and navBar done

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import BackgroundImg from '../components/BackgroundImg'
+import NavBar from '../components/NavBar'
+
+function Homepage() {
+    return (
+        <NavBar><div>Homepage</div></NavBar>
+          
+        
+      
+    )
+}
+
+export default Homepage  
