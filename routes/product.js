7565bc9d9f321c9cf7e1b4bf8f7bf928dd7876f6
Yenphuong Tong <yenphuongtong@yenphuongs-mbp.lan>
2022-08-09 01:10:58 -0700

    delete

--- routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getProducts, getProduct} = require('../controllers/productControllers')
-
-router.get('/', getProduct => {
-    res.send('Product page')
-})
-
-module.exports = router
